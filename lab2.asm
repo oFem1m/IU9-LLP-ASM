64778646928dabeef3a4e009caaa513b272b1f12
Aleksandr Volokhov <56407798+oFem1m@users.noreply.github.com>
2023-10-06 00:00:58 +0300

    lab2 added

---
+++ lab2.asm
@@ -0,0 +1,35 @@
+assume CS:code, DS:data
+
+data segment
+arr dw 0, 1, 2, 3
+len db 4
+result db -1
+element dw 0
+data ends
+
+code segment
+start:
+    mov AX, data
+    mov DS, AX
+    mov AH, 00h      
+
+    mov CL, len
+    mov SI, 0
+search:
+    mov AX, arr[SI]
+    cmp AX, element
+    je success
+    add SI, 2
+    loop search
+
+success:
+    mov AH, 02h
+    mov DL, [SI]
+    add DL, '0'
+    int 21h
+
+mov AH, 4Ch
+int 21h
+
+code ends
+end start
